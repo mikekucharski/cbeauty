98ca4da71a4b3687393a61109ac0ce05b1b0922f
mikekucharski <mikekucharski93@gmail.com>
2018-09-30 14:57:12 -0400

    Converting tabs to spaces..

--- assignments/ass1/file_reverse.c
+++ assignments/ass1/file_reverse.c
@@ -9,55 +9,55 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-	int rr;
-	int infile, outfile, filesize, seek_success;
-	char buffer[1];
+  int rr;
+  int infile, outfile, filesize, seek_success;
+  char buffer[1];
 
-	if (argc != 3) {
-		fprintf(stderr, "USAGE: %s inputFile outputFile.\n", argv[0]);
-		exit(-1);
-	}
+  if (argc != 3) {
+    fprintf(stderr, "USAGE: %s inputFile outputFile.\n", argv[0]);
+    exit(-1);
+  }
 
-	// Open the first file in read only mode.
-	if ((infile = open(argv[1], O_RDONLY)) == -1) {
-		return -1;
-	}
+  // Open the first file in read only mode.
+  if ((infile = open(argv[1], O_RDONLY)) == -1) {
+    return -1;
+  }
 
-	// Open the second file in write only mode.
-	// Create it if it doesn't exist.
-	// If it does exist truncate it to size 0.
-	if ((outfile = open(argv[2], O_WRONLY|O_CREAT|O_TRUNC, 0644)) == -1) {
-		close(infile);
-		return -2;
-	}
-	
-	filesize = lseek(infile, (off_t) 0, SEEK_END);
+  // Open the second file in write only mode.
+  // Create it if it doesn't exist.
+  // If it does exist truncate it to size 0.
+  if ((outfile = open(argv[2], O_WRONLY|O_CREAT|O_TRUNC, 0644)) == -1) {
+    close(infile);
+    return -2;
+  }
+  
+  filesize = lseek(infile, (off_t) 0, SEEK_END);
 
-	for (int i = filesize-1; i >= 0; i--) {
-		/* 
-		 * use lseek() to move the file pointer to the ith position of the input file.
-		 * To set the file pointer to a position use the SEEK_SET flag
-		 * in lseek().
-		 */
-		seek_success = lseek(infile, i, SEEK_SET);
-		if (seek_success < 0) { 
+  for (int i = filesize-1; i >= 0; i--) {
+    /* 
+     * use lseek() to move the file pointer to the ith position of the input file.
+     * To set the file pointer to a position use the SEEK_SET flag
+     * in lseek().
+     */
+    seek_success = lseek(infile, i, SEEK_SET);
+    if (seek_success < 0) { 
       return 1;
     }
 
-		rr = read(infile, buffer, 1);	/* read one byte */
-		if (rr != 1) {
-			fprintf(stderr, "Couldn't read 1 byte [%d]\n", rr);
-			exit(-1);
-		}
+    rr = read(infile, buffer, 1);	/* read one byte */
+    if (rr != 1) {
+      fprintf(stderr, "Couldn't read 1 byte [%d]\n", rr);
+      exit(-1);
+    }
 
-		rr = write(outfile, buffer, 1); /* write the byte to the file */
-		if (rr != 1) {
-			fprintf(stderr, "Couldn't write 1 byte [%d]\n", rr);
-			exit(-1);
-		}
-	}
+    rr = write(outfile, buffer, 1); /* write the byte to the file */
+    if (rr != 1) {
+      fprintf(stderr, "Couldn't write 1 byte [%d]\n", rr);
+      exit(-1);
+    }
+  }
 
-	close(infile);
-	close(outfile);
-	return 0;
+  close(infile);
+  close(outfile);
+  return 0;
 }
